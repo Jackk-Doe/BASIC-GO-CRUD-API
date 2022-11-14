4980dae1ee1e83d46f34bdd869db03c23bb99cd5
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-14 16:03:49 +1300

    Update : Post schema, to used with GET method

--- models/post.model.go
+++ models/post.model.go
@@ -1,11 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
+
+/// Old Version
+// type Post struct {
+// 	gorm.Model
+// 	ID    int `json:"id" gorm:"primary_key"`
+// 	Title string `json:"title"`
+// 	Body  string `json:"body"`
+// }
 
 type Post struct {
-	gorm.Model
-	Title string
-	Body  string
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"create_at"`
+	UpdatedAt time.Time `json:"update_at"`
 }
 
 // [PostCreate] when a Post is created from POST method
@@ -13,3 +25,13 @@ type PostCreate struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
+
+// TODO : use [PostRead] instead of [Post] to return posts data
+// [PostRead] when a server recieved query GET method
+// type PostRead struct {
+// 	ID    string `json:"id" gorm:"primary_key"`
+// 	Title string `json:"title"`
+// 	Body  string `json:"body"`
+// 	CreatedAt time.Time `json:"createAt"`
+// 	UpdatedAt time.Time `json:"updatedAt"`
+// }
