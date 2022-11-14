1a51d562faef2105572341dc324af4b1e5299e96
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-14 14:59:43 +1300

    Create : PostCreate model, for input datas

--- models/post.model.go
+++ models/post.model.go
@@ -7,3 +7,9 @@ type Post struct {
 	Title string
 	Body  string
 }
+
+// [PostCreate] when a Post is created from POST method
+type PostCreate struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
