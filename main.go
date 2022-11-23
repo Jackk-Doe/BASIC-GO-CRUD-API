2211fe352ae44a7b8b68401a7765c3ef5136b205
Jackk-Doe <dodo-stv@hotmail.com>
2022-11-23 16:48:10 +1300

    Update : router file structure

--- main.go
+++ main.go
@@ -1,42 +1,15 @@
 package main
 
 import (
-	"jackk-doe/go-crud-api/controllers"
 	"jackk-doe/go-crud-api/database"
 	"jackk-doe/go-crud-api/initializers"
-	"os"
-
-	"github.com/gin-gonic/gin"
+	"jackk-doe/go-crud-api/router"
 )
-
-func init() {
-	// Load .env & Connect to DB, init() runs before main()
-	initializers.LoadEnvVariables()
-
-	database.Init()
-}
-
-// NOTE : Suggest debug with 'CompileDaemon -command="./go-crud-api"' command
 
 func main() {
 
-	// Set Port
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+	initializers.LoadEnvVariables()
 
-	r := gin.Default()
-
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"Test": "Hello Go!"})
-	})
-
-	r.POST("/post", controllers.PostCreate)
-	r.GET("/post", controllers.PostGetAll)
-	r.GET("/post/:id", controllers.PostGetOneById)
-	r.PUT("/post/:id", controllers.PostUpdate)
-	r.DELETE("/post/:id", controllers.PostDelete)
-
-	r.Run(":" + port)
+	database.Init()
+	router.Init()
 }
